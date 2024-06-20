a0bebd95822ecbfe652d4dd54cc513bc186b1d41
Lakshay Goel <lakshaygoel9650@gmail.com>
2024-06-20 23:50:06 +0530

    Learning basics

--- Tutorial/learning-python/Ch2 - Basics/exceptions_start.py
+++ Tutorial/learning-python/Ch2 - Basics/exceptions_start.py
@@ -5,10 +5,27 @@
 
 # Errors can happen in programs, and we need a clean way to handle them
 # TODO: This code will cause an error because you can't divide by zero:
+# x= 10/0
+# print(x)
 
 # TODO: Exceptions provide a way of catching errors and then handling them in 
 # a separate section of the code to group them together
+# try:
+#     x=10/0
+# except:
+#     print("Did not work")
 
 
 # TODO: You can also catch specific exceptions
-
+try:
+    answer = input("what should i divide 10 by?")
+    num = int(answer)
+    print(10/num)
+except ZeroDivisionError as e:
+    print("Error! You can't divide by zero!")
+except ValueError as e:
+    print("you didn't give a valid number")
+    print(e)
+finally:
+    print("this code runs")    
+          
--- Tutorial/learning-python/Ch2 - Basics/variables_start.py
+++ Tutorial/learning-python/Ch2 - Basics/variables_start.py
@@ -13,25 +13,42 @@ mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
 
 # re-declaring a variable works
-
+# myint ="abc"
+# print(myint)
 # to access a member of a sequence type, use []
+print(mylist[2])
+print(mytuple[1])
 
 # use slices to get parts of a sequence
+print(mylist[1:5])
+print(mylist[1:5:2])
 
 # you can use slices to reverse a sequence
+print(mylist[::-1]) #printing reversing the array
+print(mystr[::-1]) #printing string in reverse order
 
 # dictionaries are accessed via keys
-
+print(mydict["one"])
 # ERROR: variables of different types cannot be combined
+print("string type" +str(123))  #built in function str to convert number to string
 
 # Global vs. local variables in functions
+def someFunction():
+    global mystr    #declaring mystr as global
+    mystr ="def"
+    print(mystr)
 
+someFunction()
+print(mystr) 
+
+del mystr   #deleting existing variable
+print(mystr)
