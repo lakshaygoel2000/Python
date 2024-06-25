1078935c48587a7c3f4ae577b282f20e7e37bc53
Lakshay Goel <lakshaygoel9650@gmail.com>
2024-06-25 20:17:49 +0530

    course completed

--- Tutorial/learning-python/Ch5 - Internet Data/inetdata_start.py
+++ Tutorial/learning-python/Ch5 - Internet Data/inetdata_start.py
@@ -2,8 +2,13 @@
 # Example file for retrieving data from the internet
 # LinkedIn Learning Python course by Joe Marini
 #
-
+import urllib.request
 def main():
+    weburl = urllib.request.urlopen("http://www.google.com")
+    print("result code: ", weburl.getcode())
+    data = weburl.read()
+    print(data)
+
     pass # this is a placeholder, do-nothing statement
 
 if __name__ == "__main__":
